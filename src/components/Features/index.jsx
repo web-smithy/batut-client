3e1ae8a91e355ab22c46ad57fbf00c1273c67be6
Marina Kovaleva <marinakovaleva259@gmail.com>
2022-08-30 21:06:14 +0300

    Animation for promo section

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -5,89 +5,64 @@ import chart from "../../assets/img/icon/chart.png";
 import prize from "../../assets/img/icon/prize.png";
 import target from "../../assets/img/icon/target.png";
 import task from "../../assets/img/icon/task.png";
+
 function Features() {
+  const featuresItemInfo = [
+    {
+      icon: clock,
+      title: "Lorem, ipsum dolor.",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Deleniti.",
+    },
+    {
+      icon: add,
+      title: "Lorem, ipsum dolor.",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Deleniti.",
+    },
+    {
+      icon: chart,
+      title: "Lorem, ipsum dolor.",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Deleniti.",
+    },
+    {
+      icon: prize,
+      title: "Lorem, ipsum dolor.",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Deleniti.",
+    },
+    {
+      icon: target,
+      title: "Lorem, ipsum dolor.",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Deleniti.",
+    },
+    {
+      icon: task,
+      title: "Lorem, ipsum dolor.",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Deleniti.",
+    },
+  ];
+
   return (
     <section className="features">
       <div className="container">
-        <h2>Features</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur{" "}
+        <h2 className="title-h2 features-title">Features</h2>
+        <p className="features-txt">
+          Lorem ipsum dolor sit amet consectetur
           <span className="txt--gradient">adipisicing elit.</span> Incidunt,
           ipsam.
         </p>
         <div className="features-list">
-          <div>
-            <div className="features-list__item">
-              <div className="features-list__img">
-                <img src={clock} />
+          {featuresItemInfo.map((item) => {
+            return (
+              <div>
+                <div className="features-list__item">
+                  <div className="features-list__img">
+                    <img src={item.icon} />
+                  </div>
+                  <h3>{item.title}</h3>
+                  <p>{item.text}</p>
+                </div>
               </div>
-              <h3>Lorem, ipsum dolor.</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Deleniti.
-              </p>
-            </div>
-          </div>
-          <div>
-            <div className="features-list__item">
-              <div className="features-list__img">
-                <img src={add} />
-              </div>
-              <h3>Lorem, ipsum dolor.</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Deleniti.
-              </p>
-            </div>
-          </div>
-          <div>
-            <div className="features-list__item">
-              <div className="features-list__img">
-                <img src={chart} />
-              </div>
-              <h3>Lorem, ipsum dolor.</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Deleniti.
-              </p>
-            </div>
-          </div>
-          <div>
-            <div className="features-list__item">
-              <div className="features-list__img">
-                <img src={prize} />
-              </div>
-              <h3>Lorem, ipsum dolor.</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Deleniti.
-              </p>
-            </div>
-          </div>
-          <div>
-            <div className="features-list__item">
-              <div className="features-list__img">
-                <img src={target} />
-              </div>
-              <h3>Lorem, ipsum dolor.</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Deleniti.
-              </p>
-            </div>
-          </div>
-          <div>
-            <div className="features-list__item">
-              <div className="features-list__img">
-                <img src={task} />
-              </div>
-              <h3>Lorem, ipsum dolor.</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Deleniti.
-              </p>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </section>
--- src/components/Promo/index.jsx
+++ src/components/Promo/index.jsx
@@ -24,6 +24,27 @@ function Promo() {
           />
         </div>
       </div>
+
+      <div className="banner">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+          viewBox="0 25 160 40"
+          className="waves"
+        >
+          <defs>
+            <path
+              id="wave"
+              d="M-150 53c30.77 0 59.538-20 90-20 31.077 0 60.256 20 90 20 30.77 0 59.23-20 90-20 30.77 0 59.23 20 90 20v20h-360z"
+            />
+          </defs>
+          <g>
+            <use xlinkHref="#wave" x="50" y="0" fill="#18222D" />
+            <use xlinkHref="#wave" x="50" y="2" fill="#142940" />
+            <use xlinkHref="#wave" x="50" y="4" fill="#E5E5E5" />
+          </g>
+        </svg>
+      </div>
     </section>
   );
 }
