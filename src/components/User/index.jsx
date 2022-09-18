b5b9b567bfa07d06cf7fb67b7ef8f037c474f9c2
Marina Kovaleva <marinakovaleva259@gmail.com>
2022-09-18 15:34:16 +0300

    Add avatar user

--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -1,16 +1,20 @@
 import "./style.scss";
 import React, { useContext } from "react";
 import { ContextUser } from "../store/context";
+import userPlaceholder from "../../assets/img/user_placeholder.png";
 
 function User() {
   const { user, setUser } = useContext(ContextUser);
+  const avatarImg = () => {
+    return user.photo_url || userPlaceholder;
+  };
 
   return (
     <section className="user">
       <div className="container">
         <div className="user-wrap">
           <div className="user-img">
-            <img className="user-img__avatar" src={user.photo_url} />
+            <img className="user-img__avatar" src={avatarImg()} />
           </div>
           <h4 className="title-h4 user-name">{user.first_name}</h4>
         </div>
